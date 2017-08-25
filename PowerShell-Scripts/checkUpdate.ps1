270847ff5bca75a61c8673385af3f9bd195b5ad1
BatikanHyt <batikanhyt@gmail.com>
2017-08-25 16:24:08 +0300

    Add files via upload

---
+++ PowerShell-Scripts/checkUpdate.ps1
@@ -0,0 +1,23 @@
+﻿$username = "abcd"
+$password = "secret"
+$secstr = New-Object -TypeName System.Security.SecureString
+$password.ToCharArray() | ForEach-Object {$secstr.AppendChar($_)}
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $secstr
+function checkUpd{
+#Define update criteria.
+$Criteria = "IsInstalled=0 and Type='Software'"
+
+#Search for relevant updates.
+$Searcher = New-Object -ComObject Microsoft.Update.Searcher
+$SearchResult = $Searcher.Search($Criteria).Updates
+
+If($SearchResult.Count -eq 0){
+Write-Host "No Updates Available"
+Exit
+}
+
+Write-Host "Updates Found: $($SearchResult.Count)`r`n"
+$SearchResult | ForEach-Object{Write-Host "$($_.Title) `r`n"}
+}
+
+Invoke-Command -ComputerName MachineName -ScriptBlock $function:checkUpd -Credential $cred
